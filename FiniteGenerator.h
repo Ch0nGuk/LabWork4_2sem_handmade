31cfc5ef4ca61a04a162737cff144870766c1a35
Ch0nGuk <matvey@sorokopudov.ru>
2026-06-11 14:07:34 +0300

    add Generator and FiniteGenerator

---
+++ FiniteGenerator.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "Generator.h"
+#include "Ordinal.h"
+#include "Cardinal.h"
+#include "DynamicArray.h"
+#include "Sequence.h"
+
+#include <stdexcept> // для вброса исключений
+#include <utility> // для std::move()
+
+template <class T>
+class FiniteGenerator : public Generator<T>
+{
+public:
+    FiniteGenerator() : data() {}
+
+    explicit FiniteGenerator(const DynamicArray<T>& items) : data(items) {} // explicit запрещает неявный вызов конструктора с одним аргументом. 
+
+    FiniteGenerator(const T* items, int count) : data(items, count) {}      // Поэтому FiniteGenerator<int> gen = data; не скомпилируется, 
+
+    FiniteGenerator(T* items, int count) : data(items, count) {}            // а явная запись FiniteGenerator<int> gen(data); скомпилируется. 
+
+    explicit FiniteGenerator(const Sequence<T>* sequence) : data()          // Это предотвращает случайное преобразование DynamicArray<int> в FiniteGenerator<int>.   
+    {
+        if (sequence == nullptr)
+        {
+            throw std::invalid_argument("Source sequence is null");
+        }
+
+        int count = sequence->GetLength();
+        if(count == 0) return;
+
+        DynamicArray<T> tmp_arr(count, sequence->Get(0));
+
+        for (int i = 1; i < count; i++)
+        {
+            tmp_arr[i] = sequence->Get(i);
+        }
+
+        data = std::move(tmp_arr);
+    }                                                                    
+
+    const T& Get(const Ordinal& index) const override
+    {
+        if (!index.IsFinite())
+        {
+            throw std::out_of_range("FiniteGenerator accepts only finite index");
+        }
+
+        size_t finite_index = index.GetFinitePart();
+
+        if (finite_index >= static_cast<size_t>(data.GetSize()))
+        {
+            throw std::out_of_range("Index is out of range");
+        }
+
+        return data.Get(static_cast<int>(finite_index));
+    }
+
+    const Ordinal GetOrdinalLength() const override
+    {
+        return Ordinal::Finite(static_cast<size_t>(data.GetSize()));
+    }
+
+    const Cardinal GetCardinalLength() const override
+    {
+        return Cardinal::Finite(static_cast<size_t>(data.GetSize()));
+    }
+
+    const size_t GetMaterializedCount() const override
+    {
+        return static_cast<size_t>(data.GetSize());
+    }
+
+private:
+    DynamicArray<T> data;
+};
---
+++ Generator.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef> // для size_t
+
+#include "Ordinal.h"
+#include "Cardinal.h"
+
+template <class T>
+class Generator // Виртуальный класс, интерфейс всех генераторов. Должен уметь возвращать элемент по индексу
+{               // возвращать ординальную и кардинальную длины последовательности, которую генерирует, и кол-во эл-тов в кэше
+public:
+    virtual ~Generator() {}
+
+    virtual const T& Get(const Ordinal& index) const = 0;
+    virtual const Ordinal GetOrdinalLength() const = 0;
+    virtual const Cardinal GetCardinalLength() const = 0;
+    virtual const size_t GetMaterializedCount() const = 0;
+};
